a728ecd64847bef4310e9a86e861ce14133c73e5
mdinesh7 <90355538+mdinesh7@users.noreply.github.com>
2025-09-15 21:40:40 -0400

    adding force delete variable to ecr repo to force delete when bringing down infra

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -3,9 +3,11 @@
 resource "aws_ecr_repository" "server" {
   name = "${var.project_name}/server"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true # <-- ADD THIS LINE
 }
 
 resource "aws_ecr_repository" "webapp" {
   name = "${var.project_name}/webapp"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true # <-- ADD THIS LINE
 }
